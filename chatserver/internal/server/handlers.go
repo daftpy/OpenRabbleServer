b20107a9dcb4c968e56144b2566af81b00412e49
Daftpy <daftpy@gmail.com>
2025-02-25 00:29:55 -0600

    Added a call to the hostname env variable in the discovery handler to build the chat url.

--- chatserver/internal/server/handlers.go
+++ chatserver/internal/server/handlers.go
@@ -17,6 +17,7 @@ func HandleDiscovery() http.HandlerFunc {
 		KChostname := os.Getenv("KC_HOSTNAME")
 		chatClientName := os.Getenv("CHAT_CLIENT_NAME")
 		realmName := os.Getenv("REALM_NAME")
+		hostname := os.Getenv("PUBLIC_HOSTNAME")
 
 		log.Printf("KC_HOSTNAME: %s", KChostname)
 		log.Printf("REALM_NAME: %s", realmName)
@@ -25,7 +26,7 @@ func HandleDiscovery() http.HandlerFunc {
 		response := map[string]string{
 			"auth_url":    url + "/auth",
 			"chat_client": chatClientName,
-			"chat_url":    "wss://chat.localhost/ws",
+			"chat_url":    "wss://chat." + hostname + "/ws",
 			"token_url":   url + "/token",
 			"server_name": "OnRabble",
 			"server_id":   "Placeholder",
