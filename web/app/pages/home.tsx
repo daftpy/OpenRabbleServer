6f0da7fe82a6529d3f8baa4a0a856c50608ea778
Daftpy <daftpy@gmail.com>
2025-02-26 01:23:27 -0600

    Added a ChatMessageList component to thee react web/ service. The ChannelMessageList listens for chat message events and provides a live view of the chatserver and its channels.

--- web/app/pages/home.tsx
+++ web/app/pages/home.tsx
@@ -7,9 +7,11 @@ import { useNavigate } from "react-router";
 import ChannelInput from "~/components/channel_input";
 import ChannelList from "~/components/channel_list";
 
-import { Button, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import { Button, Container, Flex, Heading, Link, Text } from "@radix-ui/themes";
 import type { Channel } from "~/components/channel_list";
 import UserList from "~/components/user_list";
+import ChatMessageList from "~/components/chat_message_list";
+import { GearIcon } from "@radix-ui/react-icons";
 
 
 /*
@@ -24,12 +26,16 @@ export function HomePage({ channels }: { channels: Channel[] }) {
     return (
       <main className="p-4">
         <Container>
-          <Flex direction="column" gap="3">
-            <div>
-              <Button onClick={() => navigate("/about")}>About</Button>
+          <Flex direction="column" gap="4">
+            <Flex direction={"column"}>
               <Heading weight={"bold"} className="text-xl">Your OnRabble Server</Heading>
-            </div>
-            <Text>Welcome to your dashboard.</Text>
+              <Text m="0">Welcome to your dashboard.</Text>
+              <Link href="#">Hide Live Chat</Link>
+            </Flex>
+            <ChatMessageList />
+            <Flex>
+              <Button onClick={() => navigate("/about")}><GearIcon /> Settings</Button>
+            </Flex>
             <div>
               <Heading weight={"bold"} style={{ color: "var(--indigo-9)" }}>Channels</Heading>
               <Text m="0">You can add a new channel or manage your channels below.</Text>
