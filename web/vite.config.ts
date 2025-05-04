9d1a677b1f09b106b10cd0199a2e6a8bfa92ec22
Daftpy <daftpy@gmail.com>
2025-05-04 00:24:10 -0500

    Changed some production settings.

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,21 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const hostname = process.env.VITE_HOSTNAME;
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  server: {
-    host: "0.0.0.0", // Exposes the dev server to Docker
-    port: 3000, // Forces Vite to use port 3000
-    strictPort: true,
-    watch: {
-      usePolling: true, // Required for Docker to detect file changes
+  return {
+    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+    server: {
+      host: "0.0.0.0", // Exposes the dev server to Docker
+      port: 3000,
+      strictPort: true,
+      watch: {
+        usePolling: true,
+      },
+      allowedHosts: [env.VITE_HOSTNAME ?? "localhost"],
     },
-    allowedHosts: [hostname ?? 'localhost'],
-  },
+  };
 });
